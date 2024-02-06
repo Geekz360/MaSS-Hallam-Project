2b9210b03c8bd816ebcc5125cc77841fb4029f2f
Geekz360 <loganmayall@yahoo.com>
2024-02-06 14:45:59 +0000

    Ver 0.0.3 - Added CSV functions, remade login

--- MaSS.py
+++ MaSS.py
@@ -1,3 +1,39 @@
+import csv
+
+# variable init
+password = ""
+username = ""
+
+# CSV rows and fields
+userFields = []
+userRows = []
+
+moduleFields = []
+moduleRows = []
+
+userModuleFields = []
+userModuleRows = []
+
+# csv readers
+userFile = open("User.csv", 'r')
+userReader = csv.reader(userFile)
+userFields = next(userReader)
+for userRow in userReader:
+	userRows.append(userRow)
+
+with open("Module.csv", 'r') as moduleFile:
+	moduleReader = csv.reader(moduleFile)
+	moduleFields = next(moduleReader)
+	for moduleRow in moduleReader:
+		moduleRows.append(moduleRow)
+
+with open("UserModule.csv", 'r') as userModules:
+	userModuleReader = csv.reader(userModules)
+	userModuleFields = next(userModuleReader)
+	for userModuleRow in userModuleReader:
+		userModuleRows.append(userModuleRow)
+
+
 # Login Systems
 def logIn():
 	print("======================== Ver 0.0.1 ==========================")
@@ -5,10 +41,12 @@ def logIn():
 	print("======================= Login Page ==========================\n")
 	username = input("Enter your username: ")
 	password = input("Enter your password: ")
-	if password == "TEST":
-		loggedIn = True
-		print("Logged in successfully")
-		return loggedIn
+	for i in range(len(userRows)):
+		if userRows[i][0] == username and userRows[i][1] == password:
+			print(userRows[i][0], userRows[i][1], userRows[i][2], userRows[i][3], userRows[i][4], userRows[i][5])
+			loggedIn = True
+			print("Logged in successfully")
+			return loggedIn and password and username
 	else:
 		print("Incorrect Username or Password!")
 		logIn()
@@ -25,7 +63,7 @@ def console(loggedIn):
 		# student main page
 		print("======================== Ver 0.0.1 ==========================")
 		print("================= Module and Student System =================")
-		print("========== Student Main Page, FULLNAME, USR TYPE ============\n")
+		print("========== Student Main Page", username, "USR TYPE ============\n")
 		print("1. View User Profile\n"
 			  "2. View All Modules\n"
 			  "3. Change Password\n"
@@ -51,7 +89,7 @@ def console(loggedIn):
 					print("Second Name: ", "TESTERSON")
 					print("User Type: ", "ADMIN")
 					print("Username: ", "TTESTERSON")
-					print("Password: ", "Test")
+					print("Password: ", )
 					print("\n7. Main Menu")
 				elif locationSelect == 2:
 					print("======================== Ver 0.0.1 ==========================")
@@ -59,6 +97,10 @@ def console(loggedIn):
 					print("============= Module Page, FULLNAME, USR TYPE ===============\n")
 					print("Available Modules - \n", "Module List")
 					print("\n7. Main Menu")
+				elif locationSelect == 3:
+					print("======================== Ver 0.0.1 ==========================")
+					print("================= Module and Student System =================")
+					print("========= Change Password Page, FULLNAME, USR TYPE ==========\n")
 
 
 console(False)
