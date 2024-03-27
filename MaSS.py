67b3c5b9f32b4216042f2435c643dfc8f562bb29
Geekz360 <loganmayall@yahoo.com>
2024-03-27 13:25:10 +0000

    Ver 0.0.5 - Added CSV writing function to enable me to change passwords

--- MaSS.py
+++ MaSS.py
@@ -1,12 +1,21 @@
 import csv
 
 # variable init
+global username
+global password
+global firstName
+global secondName
+global accountType
+global accountStatus
+global rowPlace
+
 password = ""
 username = ""
 firstName = ""
 secondName = ""
 accountType = ""
 accountStatus = ""
+rowPlace = 0
 
 # CSV rows and fields
 userFields = []
@@ -39,7 +48,6 @@ for userModuleRow in userModuleReader:
 	userModuleRows.append(userModuleRow)
 
 
-
 # Login Systems
 def logIn():
 	print("======================== Ver 0.0.1 ==========================")
@@ -51,6 +59,8 @@ def logIn():
 	global secondName
 	global accountType
 	global accountStatus
+	global rowPlace
+
 	username = input("Enter your username: ")
 	password = input("Enter your password: ")
 	for i in range(len(userRows)):
@@ -61,6 +71,11 @@ def logIn():
 			secondName = userRows[i][3]
 			accountType = userRows[i][4]
 			accountStatus = userRows[i][5]
+			rowPlace = i
+
+			if accountStatus == "Blocked":
+				print("Account is blocked! Please contact a lecturer.")
+				logIn()
 			loggedIn = True
 			print("Logged in successfully")
 			return loggedIn
@@ -69,9 +84,23 @@ def logIn():
 		logIn()
 
 
+def fileChange(editedfile, fileread):
+	with open(editedfile, 'w', newline='') as filewritten:
+		writer = csv.writer(filewritten)
+		writer.writerows(fileread)
+
+
 # Main application condensed to a function to allow recursion (logging in and out within program)
 def console(loggedIn):
 	locationSelect = int(0)
+	global username
+	global password
+	global firstName
+	global secondName
+	global accountType
+	global accountStatus
+	global rowPlace
+
 	while not loggedIn:
 
 		loggedIn = logIn()
@@ -79,13 +108,15 @@ def console(loggedIn):
 
 	else:
 		# student main page
-		print("======================== Ver 0.0.1 ==========================")
+		print("======================== Ver 0.0.4 ==========================")
 		print("================= Module and Student System =================")
 		print("============ Main Page,", firstName, secondName, ",", accountType, "============\n")
 		print("1. View User Profile\n"
 			  "2. View All Modules\n"
 			  "3. Change Password\n"
 			  "4. My Modules\n"
+			  "5. Withdraw From a Module\n"
+			  "6. Apply to a Module\n"
 			  "10. Exit")
 		while loggedIn:
 			# Student Pages
@@ -105,6 +136,8 @@ def console(loggedIn):
 						  "2. View All Modules\n"
 						  "3. Change Password\n"
 						  "4. My Modules\n"
+						  "5. Withdraw From a Module\n"
+						  "6. Apply to a Module\n"
 						  "10. Exit")
 				elif locationSelect == 10:
 					console(False)
@@ -118,6 +151,7 @@ def console(loggedIn):
 					print("Username: ", username)
 					print("Password: ", password)
 					print("\n7. Main Menu")
+
 				elif locationSelect == 2:
 					print("======================== Ver 0.0.4 ==========================")
 					print("================= Module and Student System =================")
@@ -131,6 +165,38 @@ def console(loggedIn):
 					print("======================== Ver 0.0.4 ==========================")
 					print("================= Module and Student System =================")
 					print("======= Change Password Page,", firstName, secondName + ",", accountType, "=======\n")
+					print("Username: ", username)
+					print("Password: ", password)
+					passChange = input("Would you like to change your password? (Y/N)")
+					if passChange == "N":
+						print("Exiting...")
+						print("1. View User Profile\n"
+							  "2. View All Modules\n"
+							  "3. Change Password\n"
+							  "4. My Modules\n"
+							  "5. Withdraw From a Module\n"
+							  "6. Apply to a Module\n"
+							  "10. Exit")
+					if passChange == "Y":
+						passUnchanged = 1
+						while passUnchanged == 1:
+							print("Current Password: ", password)
+							print("\n", "=============================", "\n")
+							newPassword = input("New Password: ")
+							newPasswordCheck = input("Re-enter New Password: ")
+							if newPasswordCheck == newPassword:
+								userRows[rowPlace][1] = newPassword
+								fileChange('user.csv', userRows)
+								print("1. View User Profile\n"
+									  "2. View All Modules\n"
+									  "3. Change Password\n"
+									  "4. My Modules\n"
+									  "5. Withdraw From a Module\n"
+									  "6. Apply to a Module\n"
+									  "10. Exit")
+								break
+
+
 
 				elif locationSelect == 4:
 					print("======================== Ver 0.0.4 ==========================")
@@ -148,6 +214,10 @@ def console(loggedIn):
 					print("======================== Ver 0.0.4 ==========================")
 					print("================= Module and Student System =================")
 					print("======= Withdraw Modules Page,", firstName, secondName + ",", accountType, "=======\n")
+				elif locationSelect == 6:
+					print("======================== Ver 0.0.4 ==========================")
+					print("================= Module and Student System =================")
+					print("==== Apply to Modules Page,", firstName, secondName + ",", accountType, "====\n")
 
 			# Lecturer Pages
 			elif (locationSelect == 1 or 2 or 3 or 4 or 5 or 6 or 7 or 10) and accountType == "Lecturer":
